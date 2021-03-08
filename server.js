731b997b48e488ad083030d8a84b3f1a558f4edd
Uwe Chardon <uwe@chardon.de>
2021-03-08 19:20:12 +0100

    try plesk

--- server.js
+++ server.js
@@ -1,9 +1,2 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '90a01c7cf7d7017d3f8ea5af78cf0075'),
-    },
-  },
-});
+const strapi = require('strapi');
+strapi(/* {...} */).start();
